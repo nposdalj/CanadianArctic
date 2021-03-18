33d2f74a7be855f73347551168fdb687df1f9abc
Natalie Posdaljian <nposdalj@ucsd.edu>
2021-03-17 18:37:23 -0700

    GAMS and GAMMs

--- Statistics.R
+++ Statistics.R
@@ -50,53 +50,38 @@ print(plot(viz,allTerms=T),pages=1)
 
 #first way to plot GAM
 vizGG = plot(viz,allTerms = T) +
-  l_points() +
   labs(title = 'Sperm whales (GAM)')+
   l_fitLine(linetype = 3)  +
   l_fitContour()+
   l_ciLine(mul = 5, colour = "blue", linetype = 2) +
   l_ciBar() +
-  l_points(shape = 19, size = 1, alpha = 0.1) +
   l_rug() +
   theme_get() 
 print(vizGG,pages =1)
-fig6 =paste("G:/My Drive/GofAK_TPWS_metadataReduced/SeasonalityAnalysis/",site,'/',site,"GAM1.png",sep="")
+fig6 =paste("G:/.shortcut-targets-by-id/1lwxYjZ-5ScY65o65OfWCtYU-FpMLIpXS/Arctic_Sperm whales/figures/GAM1_Allyears.png",sep="")
 ggsave(fig6)
 
 #second way to plot GAM
 vizGG2 = plot(viz, allTerms = T) +
-  l_fitLine(colour = "red") + l_rug(mapping = aes(x=x,y=y), alpha=0.8) +
+  l_fitLine(colour = "red") +
   labs(title = 'Sperm whales (GAM)')+
   l_ciLine(mul = 5, colour = "blue", linetype = 2)+
-  l_points(shape = 19, size = 1, alpha = 0.1) + theme_classic()
+  theme_classic()
 print(vizGG2,pages =1)
-fig7 =paste("G:/My Drive/GofAK_TPWS_metadataReduced/SeasonalityAnalysis/",site,'/',site,"GAM2.png",sep="")
+fig7 =paste("G:/.shortcut-targets-by-id/1lwxYjZ-5ScY65o65OfWCtYU-FpMLIpXS/Arctic_Sperm whales/figures/GAM2_Allyears.png",sep="")
 ggsave(fig7)
 
-###load all data and run GAM
-filename = paste("G:/My Drive/GofAK_TPWS_metadataReduced/SeasonalityAnalysis/All_Data.csv",sep="")
-AllTAB = read.csv(filename) #no effort days deleted
-head(AllTAB)
-str(AllTAB)
-AllTAB$Season = as.factor(AllTAB$Season) #change season from an integer to a factor
-levels(AllTAB$Season)
-AllTAB$Season = revalue(AllTAB$Season, c("1"="Summer", "2"="Fall", "3"="Winter", "4"="Spring")) #change the numbers in actual seasons
-AllTAB$tbin = anytime(as.factor(AllTAB$tbin))
+#GAMs without 2015
+DayIceHARP = subset(DayIce, Year!="2015")
 
-#remove NaNs
-AllTable = na.omit(AllTAB)
-AllTable$Site = as.character(AllTable$Site)
-
-#table with only CB, QN, PT
-Central_GOA = subset(AllTable, Site!="BD" & Site!="KS" & Site!="KOA" & Site!="AB")
-
-gamALL = gam(HoursProp ~ s(day, bs = 'cc', k = 47), data = AllTable, family = tw, method = "REML")
-plot(gamALL, pages =1)
-summary(gamALL)
+#GAM to identify seasonal pattern
+gamTw = gam(DutyBin ~ s(day, bs = 'cc', k = 15)+s(Ice)+s(Year, k = 4), data = DayIceHARP, family = tw, method = "REML")
+plot(gamTw, pages = 1)
+summary(gamTw)
 
 #Better GAM plots
 #pattern only
-viz = getViz(gamALL)
+viz = getViz(gamTw)
 print(plot(viz,allTerms=T),pages=1)
 
 #first way to plot GAM
@@ -107,18 +92,40 @@ vizGG = plot(viz,allTerms = T) +
   l_ciLine(mul = 5, colour = "blue", linetype = 2) +
   l_ciBar() +
   l_rug() +
-  theme(axis.text=element_text(size=18),
-        axis.title=element_text(size=20,face="bold"))
+  theme_get() 
 print(vizGG,pages =1)
-fig6 =paste("G:/My Drive/GofAK_TPWS_metadataReduced/SeasonalityAnalysis/GAM1.png",sep="")
+fig6 =paste("G:/.shortcut-targets-by-id/1lwxYjZ-5ScY65o65OfWCtYU-FpMLIpXS/Arctic_Sperm whales/figures/GAM1_HARPyears.png",sep="")
 ggsave(fig6)
 
 #second way to plot GAM
 vizGG2 = plot(viz, allTerms = T) +
-  l_fitLine(colour = "red") + l_rug(mapping = aes(x=x,y=y), alpha=0.8) +
+  l_fitLine(colour = "red") +
   labs(title = 'Sperm whales (GAM)')+
   l_ciLine(mul = 5, colour = "blue", linetype = 2)+
-  l_points(shape = 19, size = 1, alpha = 0.1) + theme_classic()
+  theme_classic()
 print(vizGG2,pages =1)
-fig7 =paste("G:/My Drive/GofAK_TPWS_metadataReduced/SeasonalityAnalysis/GAM2.png",sep="")
+fig7 =paste("G:/.shortcut-targets-by-id/1lwxYjZ-5ScY65o65OfWCtYU-FpMLIpXS/Arctic_Sperm whales/figures/GAM2_HARPyears.png",sep="")
+ggsave(fig7)
+
+## Generalized Additive Mixed Models
+#All years
+gam.ME = gamm(DutyBin ~ s(day, bs = 'cc', k = 15)+s(Ice)+s(Year, k = 4), data = DayIce, family = tw, method = "REML")
+plot(gam.ME[[2]], pages = 1)
+summary(gam.ME[[1]])  
+summary(gam.ME[[2]])  
+
+viz = getViz(gam.ME[[2]])
+print(plot(viz,allTerms=T),pages=1)
+fig7 =paste("G:/.shortcut-targets-by-id/1lwxYjZ-5ScY65o65OfWCtYU-FpMLIpXS/Arctic_Sperm whales/figures/GAMM_AllYears.png",sep="")
+ggsave(fig7)
+
+#HARP years
+gam.ME = gamm(DutyBin ~ s(day, bs = 'cc', k = 15)+s(Ice)+s(Year, k = 4), data = DayIceHARP, family = tw, method = "REML")
+plot(gam.ME[[2]], pages = 1)
+summary(gam.ME[[1]])  
+summary(gam.ME[[2]]) 
+
+viz = getViz(gam.ME[[2]])
+print(plot(viz,allTerms=T),pages=1)
+fig7 =paste("G:/.shortcut-targets-by-id/1lwxYjZ-5ScY65o65OfWCtYU-FpMLIpXS/Arctic_Sperm whales/figures/GAMM_HARPYears.png",sep="")
 ggsave(fig7)

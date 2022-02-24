6a23629fa996bd2754e90e49547ef33e620add35
Natalie Posdaljian <nposdalj@ucsd.edu>
2022-02-24 11:30:07 -0800

    Edited the plots

--- HistoricSeaIce.m
+++ HistoricSeaIce.m
@@ -1,53 +1,62 @@
-%Looking at Historical Sea ice data from 1850
-SaveDir = 'H:\My Drive\Manuscripts\CANARC\figures\IceComp\';
+%% This code uses historical Sea ice data from 1850 and beyond
+%NP February 2022
 %% Load Sea Ice data
-SeaIce = readtable('H:\My Drive\Manuscripts\CANARC\data\Sea Ice\1850 Onward\nsidcSeaIceConc1850_c14f_4b41_6a0b.csv');
+SaveDir = 'H:\My Drive\Manuscripts\CANARC\figures\IceComp\'; %where to save the plots
+SeaIce = readtable('H:\My Drive\Manuscripts\CANARC\data\Sea Ice\1850 Onward\nsidcSeaIceConc1850_c14f_4b41_6a0b.csv'); % load data
 SeaIce(1,:) = []; %delete first row
-%% Pond inlet Lat/Longs - 20km radius
+%% Pond inlet Lat/Longs
 LatHARP = 72.72;
 LongHARP = -76.23;
-LongHarp360 = 360 + LongHARP;
+LongHarp360 = 360 + LongHARP; %adjust it to match how the data reports long
 %% Find cells that are within the radius
 [deg,~] = distance(LatHARP,LongHarp360, SeaIce.latitude, SeaIce.longitude);
 D = deg2km(deg);
-I=find(D<20); %12 is the smallest distance away from the center that I can do, this includes the two nearest lat/lon point
-
+I=find(D<20); %20km radius around the study site
 SeaIce_Range = SeaIce(I,:); %index to find the lat/lon of interest
-
-%% Plotting points
+%% Plotting points to see how many points were selected
 UniqueLatLon = unique(SeaIce_Range(:,2:3));
-
 figure
 plot(UniqueLatLon.latitude,UniqueLatLon.longitude,'o') %USED
 hold on
 plot(LatHARP,LongHarp360,'.')
-
-%% Continuing to find cells within radius
+%% Continuing to retime the data and filter it a little more
 %Convert time to datetime
 b=regexp(SeaIce_Range.time,'\T','split');
 b=[b{:}];
 B=datetime(b(1:2:end).','InputFormat','yyyy-MM-dd');
 SeaIce_Range.date = B;
+[SeaIce_Range.yr,~,~] = ymd(SeaIce_Range.date); %add just the year
 
-%Average months
-SeaIce_Range = table(SeaIce_Range.date, SeaIce_Range.latitude, SeaIce_Range.longitude, SeaIce_Range.seaice_conc);
+%Average the months that are the same and rename the table
+SeaIce_Range = table(SeaIce_Range.date, SeaIce_Range.latitude, SeaIce_Range.longitude, SeaIce_Range.seaice_conc, SeaIce_Range.yr);
+allVars = 1:width(SeaIce_Range);
+newNames = {'date','lat','lon','concentration','yr'};
+SeaIce_Range =renamevars(SeaIce_Range,allVars,newNames);
 SeaIce_Range = table2timetable(SeaIce_Range);
 SeaIce_Avg = retime(SeaIce_Range,'monthly','mean');
-allVars = 1:width(SeaIce_Avg);
-newNames = {'lat','lon','concentration'};
-SeaIce_Avg =renamevars(SeaIce_Avg,allVars,newNames);
 
-%Find the number of months each year with zero sea ice concentration
+% Cutting the data before 1979
+IDXyr = find(SeaIce_Avg.yr >= 1979);
+SeaIce_Avg_1979 = SeaIce_Avg(IDXyr,:);
+
+%Find the number of months each year with zero sea ice concentration (Full
+%data)
 Zeros = find(SeaIce_Avg.concentration==0);
 ZeroTable = SeaIce_Avg(Zeros,:);
 ZeroTable = timetable2table(ZeroTable);
-[ZeroTable.Yr,~,~] = ymd(ZeroTable.Var1);
+
+%Find the number of months each year with zero sea ice concentration (1979
+%and beyond)
+Zeros = find(SeaIce_Avg_1979.concentration==0);
+ZeroTable_1979 = SeaIce_Avg_1979(Zeros,:);
+ZeroTable_1979 = timetable2table(ZeroTable_1979);
 
 %Find the minimum for each year
 SeaIce_YearMin = retime(SeaIce_Avg,'yearly','min');
+SeaIce_YearMin_1979 = retime(SeaIce_Avg_1979,'yearly','min');
 %% Plot for historical sea ice
 figure
-plot(SeaIce_Avg.Var1,SeaIce_Avg.concentration);
+plot(SeaIce_Avg.date,SeaIce_Avg.concentration);
 ylabel('Sea Ice Concentration')
 xlabel('Time')
 title('Monthly Sea Ice Concentration near Pond Inlet')
@@ -56,7 +65,7 @@ weeklyfn = 'HistoricalSeaIce_TimeSeries';
 saveas(gcf,fullfile(SaveDir,weeklyfn),'png')
 
 figure
-plot(SeaIce_YearMin.Var1,SeaIce_YearMin.concentration,'.');
+plot(SeaIce_YearMin.date,SeaIce_YearMin.concentration,'.');
 ylabel('Sea Ice Concentration')
 xlabel('Year')
 title('Minimum Sea Ice Concentration near Pond Inlet for Each Year')
@@ -66,7 +75,7 @@ weeklyfn = 'HistoricalSeaIce_Minima';
 saveas(gcf,fullfile(SaveDir,weeklyfn),'png')
 
 figure
-hist(ZeroTable.Yr)
+hist(ZeroTable.yr)
 xlabel('Decade')
 ylabel('Months')
 title('Number of Months with a Sea Ice Concentration of Zero')
@@ -74,9 +83,13 @@ title('Number of Months with a Sea Ice Concentration of Zero')
 weeklyfn = 'HistoricalSeaIce_Hist';
 saveas(gcf,fullfile(SaveDir,weeklyfn),'png')
 
-%Group data by year
+%Group data by year (full data)
 ZeroTableYr = retime(table2timetable(ZeroTable),'yearly','count');
-bar(ZeroTableYr.Var1,ZeroTableYr.concentration)
+bar(ZeroTableYr.date,ZeroTableYr.concentration)
+
+%Group data by year (1979 and beyond data)
+ZeroTableYr_1979 = retime(table2timetable(ZeroTable_1979),'yearly','count');
+bar(ZeroTableYr_1979.date,ZeroTableYr_1979.concentration)
 %% Compare with JJ's Sea Ice
 SeaIce_20km = readtable('H:\My Drive\Manuscripts\CANARC\data\Sea Ice\CANARC_PI_2012-2021_20km_landMask_stats_datestr_fromJJ.csv');
 SeaIce_20km = table2timetable(SeaIce_20km);
@@ -128,7 +141,7 @@ saveas(gcf,fullfile(SaveDir,weeklyfn),'png')
 %% Plot Sea Ice Concentration Over one Another
 %Monthly with Daily
 figure
-plot(SeaIce_Avg.Var1,SeaIce_Avg.concentration);
+plot(SeaIce_Avg.date,SeaIce_Avg.concentration);
 hold on
 plot(SeaIce_20km.Date,SeaIce_20km.Mean);
 ylabel('Sea Ice Concentration')
@@ -141,7 +154,7 @@ saveas(gcf,fullfile(SaveDir,weeklyfn),'png')
 
 %Monthly with Daily zoomed in on time period with overlap
 figure
-plot(SeaIce_Avg.Var1,SeaIce_Avg.concentration);
+plot(SeaIce_Avg.date,SeaIce_Avg.concentration);
 hold on
 plot(SeaIce_20km.Date,SeaIce_20km.Mean);
 ylabel('Sea Ice Concentration')
@@ -155,7 +168,7 @@ saveas(gcf,fullfile(SaveDir,weeklyfn),'png')
 
 %Monthly with Monthly
 figure
-plot(SeaIce_Avg.Var1,SeaIce_Avg.concentration);
+plot(SeaIce_Avg.date,SeaIce_Avg.concentration);
 hold on
 plot(SeaIce_20km_Month.Date,SeaIce_20km_Month.Mean);
 ylabel('Sea Ice Concentration')
@@ -168,7 +181,7 @@ saveas(gcf,fullfile(SaveDir,weeklyfn),'png')
 
 %Monthly with Monthly
 figure
-plot(SeaIce_Avg.Var1,SeaIce_Avg.concentration);
+plot(SeaIce_Avg.date,SeaIce_Avg.concentration);
 hold on
 plot(SeaIce_20km_Month.Date,SeaIce_20km_Month.Mean);
 ylabel('Sea Ice Concentration')
@@ -182,9 +195,10 @@ saveas(gcf,fullfile(SaveDir,weeklyfn),'png')
 
 %% Plot yearly median - all the data
 SeaIce_Median = retime(SeaIce_Avg,'yearly', @(x) median(x,'omitnan'));
+SeaIce_Median_1979 = retime(SeaIce_Avg_1979,'yearly', @(x) median(x,'omitnan'));
 
 figure
-plot(SeaIce_Median.Var1,SeaIce_Median.concentration,'o')
+plot(SeaIce_Median.date,SeaIce_Median.concentration,'o')
 xlabel('Year')
 ylabel('Median Sea Ice Concentration')
 title('Yearly Median Sea Ice Concentration')
@@ -192,15 +206,12 @@ title('Yearly Median Sea Ice Concentration')
 weeklyfn = 'Yearly_Median_SIC';
 saveas(gcf,fullfile(SaveDir,weeklyfn),'png')
 
-[SeaIce_Median.Yr,~,~] = ymd(SeaIce_Median.Var1);
-[SeaIce_Avg.Yr,~,~] = ymd(SeaIce_Avg.Var1);
-
 figure
-boxplot(SeaIce_Avg.concentration,categorical(SeaIce_Avg.Yr))
+boxplot(SeaIce_Avg.concentration,categorical(SeaIce_Avg.yr))
 figure
-violinplot(SeaIce_Avg.concentration,categorical(SeaIce_Avg.Yr))
+violinplot(SeaIce_Avg.concentration,categorical(SeaIce_Avg.yr))
 %% Plot decadal median 
-[groups,groupID] = findgroups(floor(SeaIce_Avg.Yr/10)*10); %group by decade
+[groups,groupID] = findgroups(floor(SeaIce_Avg.yr/10)*10); %group by decade
 SeaIce_Avg.group = groups;
 decMedian = splitapply(@median,SeaIce_Avg.concentration,groups);
 plot(groupID,decMedian,'o')
@@ -225,10 +236,17 @@ weeklyfn = 'Decadal_MedianSIC_ViolinPlots';
 saveas(gcf,fullfile(SaveDir,weeklyfn),'png')
 
 %% Take decadal data and find min per year
+%Full data
 Zeros = find(SeaIce_Avg.concentration==0);
 ZeroTable = SeaIce_Avg(Zeros,:);
 ZeroTable = timetable2table(ZeroTable);
-[ZeroTable.Yr,~,~] = ymd(ZeroTable.Var1);
+[ZeroTable.yr,~,~] = ymd(ZeroTable.date);
+
+%1979 and beyond
+Zeros = find(SeaIce_Avg_1979.concentration==0);
+ZeroTable_1979 = SeaIce_Avg_1979(Zeros,:);
+ZeroTable_1979 = timetable2table(ZeroTable_1979);
+[ZeroTable_1979.yr,~,~] = ymd(ZeroTable_1979.date);
 
 %% Save tables
 writetimetable(SeaIce_Avg,'H:\My Drive\Manuscripts\CANARC\data\Sea Ice\SeaIce_Range_Avg.csv');
--- HistoricSeaIce_Decomposition.R
+++ HistoricSeaIce_Decomposition.R
@@ -8,12 +8,19 @@ library(tidyr)
 library(lmtest)
 library(data.table)
 library(tidyr)
+library(ggbreak)
+library(patchwork)
 
 #Load Sea Ice Data from Matlab (Averaged by source)
 SeaIce = read.csv('H:/My Drive/Manuscripts/CANARC/data/Sea Ice/SeaIce_Range_Avg.csv')
 Min = read.csv('H:/My Drive/Manuscripts/CANARC/data/Sea Ice/SeaIce_Range_Zero.csv')
-SeaIce = subset(SeaIce,select = c(Var1,concentration))
 
+#Create timeseries with 1979 and beyond
+SeaIce_1979 = SeaIce[which(SeaIce$yr >= 1979),]
+
+## FULL DATA
+#Create ts for full data
+SeaIceTS = subset(SeaIce,select = c(date,concentration))
 SeaIceTS = ts(SeaIce$concentration,frequency = 12, start = c(SeaIce$concentration[1],1)) #Convert it to a ts
 
 #Decompose and plot
@@ -38,20 +45,48 @@ plot(SeaIceTS, type="l")  # original series
 plot(ts.sa, type="l")  # seasonal adjusted
 seasonplot(ts.sa, 12, col=rainbow(12), year.labels=TRUE, main="Seasonal plot: SIC") # seasonal frequency set as 12 for monthly data.
 
-#Load Median SIC data from Matlab
+## SATELLITE DATA ONLY
+#Create ts for 1979 data
+SeaIceTS = subset(SeaIce_1979,select = c(date,concentration))
+SeaIceTS = ts(SeaIce_1979$concentration,frequency = 12, start = c(SeaIce_1979$concentration[1],1)) #Convert it to a ts
+
+#Decompose and plot
+decomposedRes <- decompose(SeaIceTS,"additive") # use type = "additive" for additive components
+plot(decomposedRes)
+stlRes <- stl(SeaIceTS, s.window = "periodic")
+
+#Plot only the decomposed trend
+t <- melt(decomposedRes$trend)
+t <- t%>% drop_na(value)
+t$time<-c(1:nrow(t)) 
+linear<-lm(value ~time, data=t)
+coeftest(linear)
+cor(t$value,predict(linear))
+
+trModel <- lm(SeaIce$concentration ~ c(1:length(SeaIce$concentration)))
+plot(resid(trModel), type="l")  # resid(trModel) contains the de-trended series.
+
+ts.stl <- stl(SeaIceTS,"periodic")  # decompose the TS
+ts.sa <- seasadj(ts.stl)  # de-seasonalize
+plot(SeaIceTS, type="l")  # original series
+plot(ts.sa, type="l")  # seasonal adjusted
+seasonplot(ts.sa, 12, col=rainbow(12), year.labels=TRUE, main="Seasonal plot: SIC") # seasonal frequency set as 12 for monthly data.
+
+###################################
+#Load Median SIC data from Matlab (averaged by source)
 SeaIceMed = read.csv('H:/My Drive/Manuscripts/CANARC/data/Sea Ice/SeaIce_Range_Median.csv')
-SeaIceMed_Comp = na.omit(SeaIceMed)
-plot(SeaIceMed_Comp$Yr,SeaIceMed_Comp$concentration)
+
+## FULL DATA
+plot(SeaIceMed$yr,SeaIceMed$concentration)
 
 #Linear Model
-trModel <- lm(SeaIceMed_Comp$concentration ~ c(1:length(SeaIceMed_Comp$concentration)))
+trModel <- lm(SeaIceMed$concentration ~ c(1:length(SeaIceMed$concentration)))
 plot(resid(trModel), type="l")  # resid(trModel) contains the de-trended series.
 
 #### FOR PUBLICATION #######
-
 #Linear Model
-LM_SIC = lm(formula = concentration~Yr,data = SeaIceMed)
-ggplot(SeaIceMed, aes(x=Yr, y=concentration))+
+LM_SIC = lm(formula = concentration~yr,data = SeaIceMed)
+ggplot(SeaIceMed, aes(x=yr, y=concentration))+
   geom_point() + geom_smooth(method="lm", col="black") + 
   stat_regline_equation(label.x = 1985, label.y = 84) + theme_bw() +
   labs(title = "Yearly Median Sea Ice Concentration",
@@ -60,5 +95,33 @@ ggplot(SeaIceMed, aes(x=Yr, y=concentration))+
                                                            axis.title=element_text(size=16,face="bold"))
 
 #Histogram of minimum
-Min = ggplot(Min, aes(x=Yr)) + geom_histogram(binwidth = 10)
-Min
+MinPlot = ggplot(Min, aes(x=yr)) + geom_histogram(binwidth = 10)
+MinPlot
+
+## 1901 AND BEYOND
+SeaIceMed_1901 = SeaIceMed[which(SeaIceMed$yr >= 1901),]
+Min_1901 = Min[which(Min$yr >= 1901),]
+#Linear Model
+LM_SIC = lm(formula = concentration~yr,data = SeaIceMed_1901)
+summary(LM_SIC)
+
+#### PLOTS FOR PUBLICATION #######
+ggplot(SeaIceMed_1901, aes(x=yr, y=concentration))+
+  geom_point() + geom_smooth(method="lm", col="black") + theme_bw()+
+  stat_regline_equation(label.x = 1990, label.y = 92)+
+  labs(x = "Year",
+       y = "Median Sea Ice Concentration (%)")+ theme(axis.text=element_text(size=15),
+                                                  axis.title=element_text(size=16,face="bold"))+
+  ggtitle("Yearly Median Mid-Month Day Sea Ice Concentration") +
+  theme(plot.title = element_text(hjust = 0.5,size=16,face="bold"))
+
+#histogram
+decades = c("1910","1930","1950","1970","1990","2010")
+decbreaks = c(7,9,11,13,15,17)
+ggplot(Min_1901, aes(x=group)) + geom_histogram() + geom_bar(width = 0.9) + theme_bw()+scale_x_continuous(breaks=decbreaks, labels=decades)+
+  labs(x = "Decades",
+       y = "Number of Months")+ theme(axis.text=element_text(size=15),
+                                                      axis.title=element_text(size=16,face="bold"))+
+  ggtitle("Number of Months with a Mid-Month Day \n Average Sea Ice Concentration of Zero") +
+  theme(plot.title = element_text(hjust = 0.5,size=16,face="bold"))
+
